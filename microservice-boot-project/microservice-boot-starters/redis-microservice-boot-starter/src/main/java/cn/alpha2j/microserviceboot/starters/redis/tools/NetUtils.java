94a559a6377c9ee0bb0a8e2d55f97bf3147f3bef
Jeb.Wang <jeb.wannng@gmail.com>
2024-03-01 19:02:23 +0800

    update 这个是可以部署成功的

---
+++ microservice-boot-project/microservice-boot-starters/redis-microservice-boot-starter/src/main/java/cn/alpha2j/microserviceboot/starters/redis/tools/NetUtils.java
@@ -0,0 +1,7 @@
+package cn.alpha2j.microserviceboot.starters.redis.tools;
+
+public class NetUtils {
+    public static String hello() {
+        return "world";
+    }
+}
---
+++ microservice-boot-project/microservice-boot-starters/redis-microservice-boot-starter/src/main/java/cn/alpha2j/microserviceboot/starters/redis/tools/TimeUtils.java
@@ -0,0 +1,4 @@
+package cn.alpha2j.microserviceboot.starters.redis.tools;
+
+public class TimeUtils {
+}
